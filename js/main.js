05c37a1667a1c867c1ad562a235d12c71e3907d8
natalete <natsalete14@gmail.com>
2024-09-24 14:19:38 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#carousel-imagens").slick({
+        autoplay: true,
+        arrows: false,
+        // dots: true,
+        // infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+})
