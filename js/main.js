6e01ed41887ac074b13be7343099a353f2916aab
natalete <natsalete14@gmail.com>
2024-09-27 15:14:00 -0300

    fix: correções para mobile

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ $(document).ready(function () {
     adaptiveHeight: true,
   });
 
-  $(".menu-hamburguer").click(function () {
+  $(".menu-hamburguer").on(" click touchstart", function () {
     $("nav").slideToggle();
   });
 
-  $(".container-nav li a").click(function (e) {
+  $(".container-nav li a").on(" click touchstart",function (e) {
     e.preventDefault(); // Evita o comportamento padrão do link
 
     // Pega o valor do atributo href do link clicado (o destino)
@@ -31,13 +31,13 @@ $(document).ready(function () {
 
   const contadorFavoritos = $(".contador-favoritos");
 
-  $(".img-coracao").click(function () {
+  $(".img-coracao").on(" click touchstart", function () {
     $(this).css("display", "none");
     $(this).next(".img-coracaoVermelho").css("display", "block");
     contadorFavoritos.text(parseInt(contadorFavoritos.text()) + 1);
   });
 
-  $(".img-coracaoVermelho").click(function () {
+  $(".img-coracaoVermelho").on(" click touchstart", function () {
     $(this).css("display", "none");
     $(this).prev(".img-coracao").css("display", "block");
     let valorAtual = parseInt(contadorFavoritos.text());
@@ -46,7 +46,7 @@ $(document).ready(function () {
 
   const contadorCarinho = $(".contador-carinho");
 
-  $(".button-carinho").click(function () {
+  $(".button-carinho").on(" click touchstart", function () {
     contadorCarinho.text(parseInt(contadorCarinho.text()) + 1);
   });
 
